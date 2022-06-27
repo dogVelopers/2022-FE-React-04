3b27d1458cd95a5b25dac24a786496e09ad643ab
Hyesung Oh <haesungoh414@gmail.com>
2022-06-27 16:00:02 +0900

    0626 스터디 진행

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+
 function App() {
+  const [number, setNumber] = useState<number>(1);
+
   return (
     <div className="App">
-      <h1>안녕하세요</h1>
+      <StyledH1 num={number} backgroundColor="green">
+        안녕하세요
+      </StyledH1>
+      <p>{number}</p>
+      <button onClick={() => setNumber(prev => prev + 1)}>up</button>
     </div>
   );
 }
 
 export default App;
+
+interface StyledH1Props {
+  num: number;
+  backgroundColor: string;
+}
+
+const StyledH1 = styled.h1<StyledH1Props>`
+  /* color: ${props => (props.num % 2 === 0 ? 'red' : 'blue')}; */
+  color: ${({ num }) => (num % 2 == 0 ? 'red' : 'blue')};
+  background-color: ${({ backgroundColor }) => backgroundColor};
+`;
